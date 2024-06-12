cd6a734cd66a0d648785cc92bd6ea940d70bb0ec
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-12 18:59:54 -0300

    Add files via upload

--- barrio.h
+++ barrio.h
@@ -2,27 +2,79 @@
 #define BARRIO_H_
 
 #include "parada.h"
-#include "lista.h"
+#include "Lista.h"
 #include <string>
 
-
+template <typename Parada>
 class Barrio {
-private:
+    
+    private:
 
-    Lista<Parada>* paradas;
-    std::string nombre;
+        Lista<Parada>* paradas;
+        std::string nombre;
 
-public:
+    public:
 
-    Barrio(int longitud, std::string nombre, Parada dato_inicial);
-    virtual ~Barrio();
-    Parada& obtenerParada(int posicion);
-    int agregarParada(Parada dato);
-    int contarParadas();
-    bool estaVacio();
-    void asignarParada(int posicion, Parada dato);
-    void removerParada(int posicion);
-    void recorrerParadas();
+        Barrio(int longitud, std::string nombre, Parada dato_inicial) {
+
+            if (longitud < 1) {
+
+                //throw invalid_argument("La longitud debe ser mayor o igual a 1");
+
+            } else {
+
+                this->nombre = nombre;
+                this->paradas = new Lista<Parada>(longitud, dato_inicial);
+
+            }        
+
+        }
+
+        virtual ~Barrio() {
+
+            delete this->paradas;
+
+        }
+
+        Parada& obtenerParada(int posicion) {
+
+            return this->paradas->obtener(posicion);
+
+        }
+
+        int agregarParada(Parada dato) {
+
+            return this->paradas->agregar(dato);
+
+        }
+        int contarParadas() {
+
+            return this->paradas->contarElementos();
+
+        }
+        bool estaVacio() {
+
+            return this->paradas->estaVacio();
+
+        }
+
+        void asignarParada(int posicion, Parada dato) {
+
+            this->paradas->asignar(posicion, dato);
+
+        }
+        
+        void removerParada(int posicion) {
+
+            this->paradas->remover(posicion);
+
+        }
+
+        void recorrerParadas() {
+
+            this->paradas->recorrer();
+
+        }
 
 };
 
