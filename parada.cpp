6013bf4cc1d144eb25cead608b983b136e78feeb
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-20 16:02:50 -0300

    Update parada.cpp

--- parada.cpp
+++ parada.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-Parada::Parada(string direccion, int colectivo) {
+Parada::Parada(string direccion, int colectivo, double lat, double lon) {
 
     if (colectivo < 1) {
 
@@ -15,6 +15,8 @@ Parada::Parada(string direccion, int colectivo) {
 
         this->direccion = direccion;
         this->colectivo = colectivo;
+        this->lat = lat;
+        this->lon = lon;
 
     }
 
@@ -32,6 +34,18 @@ int Parada::obtenerColectivo() {
     
 }
 
+double Parada::obtenerLatitud() {
+
+    return this->lat;
+    
+}
+
+double Parada::obtenerLongitud() {
+
+    return this->lon;
+    
+}
+
 void Parada::cambiarDireccion(string direccion) {
 
     this->direccion = direccion;
@@ -42,10 +56,4 @@ void Parada::cambiarColectivo(int colectivo) {
 
     this->colectivo = colectivo;
     
-}
-
-Parada::~Parada() {
-
-
-
 }
