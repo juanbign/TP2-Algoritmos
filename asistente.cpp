64ec0fd89142632f95016955834e2fc7fce61d2e
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-20 17:51:48 -0300

    Update asistente.cpp

--- asistente.cpp
+++ asistente.cpp
@@ -16,6 +16,8 @@ int Asistente::obtenerMaximoNumeroColectivo() {
 Lista<Parada>* Asistente::obtenerParadasDeColectivo(Lista<Barrio<Parada> >* barrios, int colectivo) {
 		
     Lista<Parada>* paradasPorLinea = new Lista<Parada>;
+
+    // Obtengo el primer barrio para no tener que crear una lista de Paradas solo para inicializar
     Barrio<Parada> barrio = barrios->obtener(1);
     
     for (int i = 1; i < barrios->getTamanio() + 1; i++) {
@@ -24,8 +26,10 @@ Lista<Parada>* Asistente::obtenerParadasDeColectivo(Lista<Barrio<Parada> >* barr
 
         for (int j = 1; j < barrio.contarParadas(); j++) {
 
-            if (barrio.obtenerParada(j).obtenerColectivo() == colectivo) {
+            // Si el colectivo es igual al pasado por parámetro
+            if (barrio.obtenerParada(j).obtenerColectivo() == colectivo) { 
 
+                // Agrego la parada a la lista
                 paradasPorLinea->agregar(barrio.obtenerParada(j), paradasPorLinea->getTamanio() + 1);
 
             }
@@ -42,8 +46,11 @@ Lista<int>* Asistente::obtenerCantidadParadasPorColectivo(Lista<Barrio<Parada> >
 
     Lista<int>* colectivos = new Lista<int>;
 
+    // Recorro todos los colectivos desde 1 a MAXIMO_NUM_LINEA_COLECTIVO + 1 (195) 
     for (int i = 1; i < MAXIMO_NUM_LINEA_COLECTIVO + 1; i++) {
 
+        // Agrego el tamaño de la lista de paradas del colectivo
+        // Si no se encuentra ninguna parada el valor es 0
         colectivos->agregar(obtenerParadasDeColectivo(barrios, i)->getTamanio(), i);
 
     }
@@ -58,10 +65,13 @@ Lista<int>* Asistente::obtenerCantidadParadasPorColectivo(Lista<Barrio<Parada> >
 */
 Parada Asistente::obtenerParadaMasCercana(Lista<Barrio<Parada> >* barrios, double lat, double lon) {
 
-    double distancia = numeric_limits<double>::max();
+    // Numero máximo del tipo "double"
+    double min_distancia = numeric_limits<double>::max();
     double nueva_distancia;
-    Parada parada = Parada("placeholder", 1, 0, 0);
+    Parada min_parada = Parada("placeholder", 1, 0, 0);
     Parada nueva_parada = Parada("placeholder", 1, 0, 0);
+    
+    // Obtengo el primer barrio para no tener que crear una lista de Paradas solo para inicializar
     Barrio<Parada> barrio = barrios->obtener(1);
 
     for (int i = 1; i < barrios->getTamanio() + 1; i++) {
@@ -71,12 +81,16 @@ Parada Asistente::obtenerParadaMasCercana(Lista<Barrio<Parada> >* barrios, doubl
         for (int j = 1; j < barrios->obtener(i).contarParadas() + 1; j++) {
 
             nueva_parada = barrio.obtenerParada(j);
-            nueva_distancia = sqrt(pow(parada.obtenerLatitud() - lat, 2) + pow(parada.obtenerLongitud() - lon, 2));
             
-            if (nueva_distancia < distancia) {
+            // Obtengo la distancia con pitágoras 
+            nueva_distancia = sqrt(pow(nueva_parada.obtenerLatitud() - lat, 2) + pow(nueva_parada.obtenerLongitud() - lon, 2));
+            
+            // Si la nueva distancia es menor a la mínima
+            if (nueva_distancia < min_distancia) {
 
-                distancia = nueva_distancia;
-                parada = nueva_parada;
+                // Igualo al nuevo mínimo
+                min_distancia = nueva_distancia;
+                min_parada = nueva_parada;
 
             }
 
@@ -84,8 +98,8 @@ Parada Asistente::obtenerParadaMasCercana(Lista<Barrio<Parada> >* barrios, doubl
 
     }
 
-    return parada;
-
+    return min_parada;
+    
 }
 
 Lista<Parada>* Asistente::obtenerParadasOrdenadasPorDistancia(Barrio<Parada> barrio, int colectivo, double lat, double lon) {
@@ -97,6 +111,7 @@ Lista<Parada>* Asistente::obtenerParadasOrdenadasPorDistancia(Barrio<Parada> bar
 
         parada = barrio.obtenerParada(i);
         
+        // Si el colectivo es igual al pasado por pará metro
         if (parada.obtenerColectivo() == colectivo) { 
         
             listaParadas->agregar(parada, listaParadas->getTamanio() + 1);
