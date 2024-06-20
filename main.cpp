04c09f0737cc3b9fb7a49f586a13362475b02c72
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-20 16:01:16 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include "lista.h"
+#include "parada.h"
+#include "barrio.h"
+#include "asistente.h"
 #include <iostream>
 #include <fstream>
 
-using namespace std; 
+using namespace std;
 
 int main (int argc, char** argv) {
 
-    
-    
+    Asistente asistente = Asistente();
+
+    Lista<Parada>* paradas = new Lista<Parada>;
+    Parada parada = Parada("Diaz Colodrero 3195", 71, -34.56713986016535, -58.49069929103298);
+    paradas->agregar(parada, paradas->getTamanio() + 1);
+    parada = Parada("Diaz Colodrero 3495", 71, -34.56382815931894, -58.49316661007587);
+    paradas->agregar(parada, paradas->getTamanio() + 1);
+    Barrio<Parada> barrio = Barrio<Parada>(2, "Villa Urquiza", parada);
+    barrio.agregarParada(paradas->obtener(1));
+    barrio.agregarParada(paradas->obtener(2));
+    asistente.mostrarParadasOrdenadasPorDistancia(barrio, 71, -34, -58);
 
 
 }
