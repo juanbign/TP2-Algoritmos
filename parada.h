75e2d34b0d6f38b20f9a96b1a47d43a71a14e178
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-20 16:02:38 -0300

    Update parada.h

--- parada.h
+++ parada.h
@@ -8,17 +8,21 @@ private:
 
     std::string direccion;
     int colectivo;
-    int lat;
-    int lon;
+    double lat;
+    double lon;
 
 public:
 
-    Parada(std::string direccion, int colectivo);
-    virtual ~Parada();
+    Parada(std::string direccion, int colectivo, double lat, double lon);
+    Parada() : direccion(""), colectivo(0), lat(0.0), lon(0.0) {}
     std::string obtenerDireccion();
     int obtenerColectivo();
+    double obtenerLatitud();
+    double obtenerLongitud();
     void cambiarDireccion(std::string direccion);
     void cambiarColectivo(int colectivo);
+    void cambarLatitud();
+    void cambiarLongitud();
 
 };
 
