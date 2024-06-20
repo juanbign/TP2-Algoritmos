66fea8b2665e16b49083f5be4fb71d6d591edb92
juanbign <92689436+juanbign@users.noreply.github.com>
2024-06-20 16:01:34 -0300

    Update Asistente.h

--- Asistente.h
+++ Asistente.h
@@ -1,12 +1,10 @@
-/*
- * Asistente.h
- *
- *  Created on: 19 jun. 2024
- *      Author: martin
- */
-#include "Lista.h"
+#include "lista.h"
 #include "parada.h"
 #include "barrio.h"
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <cmath>
 
 #ifndef ASISTENTE_H_
 #define ASISTENTE_H_
@@ -14,73 +12,107 @@
 class Asistente{
 
 
+    public:
+
 /******************************************************************************************************************
  * Primera aproximación, falta pruebas
  ******************************************************************************************************************/
 
 	/**
 	 * pre:
-	 * post: Agrega las paradas de un determinado barrio y linea de colectivo a la lista de paradas
-	 * de una deteminada linea de colectivo de todos los barrios de la ciudad
+	 * post: Devuelve una lista de todas las paradas de una linea de colectivo en un barrio 
 	 */
 
-	void obtenerParadasPorLinea(int lineaColectivo, Barrio barrio, Lista<Parada>* paradasPorLinea){
+	Lista<Parada>* obtenerParadasPorColectivoYBarrio(Lista<Barrio<Parada> >* barrios, int colectivo, std::string nombreBarrio){
 
-		Lista<Parada>* paradas = barrio.obtenerParadas();
+		Lista<Parada>* paradasPorLinea = new Lista<Parada>;
+        Barrio<Parada> barrio = barrios->obtener(1);
+		
+        for (int i = 1; i < barrios->getTamanio() + 1; i++) {
 
-		paradas ->iniciarCursor();
-		while(paradas->avanzarCursor()){
-			Parada parada = paradas.obtenerCursor();
-			if (parada->obtenerLineaColetivo() == lineaColectivo){
-				paradasPorLinea->agregarNodoFinal(parada);
+            barrio = barrios->obtener(i);
 
-			}
-		}
+            if (barrio.obtenerNombre() != nombreBarrio) {
+
+                continue;
+
+            }
+
+            for (int j = 1; j < barrio.contarParadas(); j++) {
+
+                if (barrio.obtenerParada(j).obtenerColectivo() == colectivo) {
+
+                    paradasPorLinea->agregar(barrio.obtenerParada(j), paradasPorLinea->getTamanio() + 1);
+
+                }
+
+            }
+
+            break;
+
+        }
+		
+		return paradasPorLinea;
 
 	}
    /**
-    * pre: Cada parada de colectivo tiene asociada una sola linea de colectivos
-    * post: Devuelve una lista de paradas, con todas las paradas de una deteminada linea de colectivo
-    * de todos los barrios de la ciudad
+    * pre: 
+    * post: Devuelve una lista de todas las paradas de una linea de colectivo en todos los barrios 
     */
-	Lista<Parada>* obtenerParadasPorLinea(int lineaColectivo,Lista<Barrio>* barrios){
+	Lista<Parada>* obtenerParadasPorColectivo(Lista<Barrio<Parada> >* barrios, int colectivo) {
 		
-		Lista<Parada>* paradasPorLinea = new Lista<Parada*>;
-		barrios -> iniciarCursor();
+		Lista<Parada>* paradasPorLinea = new Lista<Parada>;
+        Barrio<Parada> barrio = barrios->obtener(1);
 		
-		while(barrios->avanzarCursor()){
-			
-			Barrio* barrio = barrios->obtenerCursor();
-			paradaPorLinea(lineaColectivo,barrio,paradasPorLinea);
+        for (int i = 1; i < barrios->getTamanio() + 1; i++) {
 
-			return paradasPorLinea;
+            barrio = barrios->obtener(i);
 
-		}
+            for (int j = 1; j < barrio.contarParadas(); j++) {
+
+                if (barrio.obtenerParada(j).obtenerColectivo() == colectivo) {
+
+                    paradasPorLinea->agregar(barrio.obtenerParada(j), paradasPorLinea->getTamanio() + 1);
+
+                }
+
+            }
+
+        }
+		
+		return paradasPorLinea;
 
 	}
+
 	/**
 	 * pre
-	 * post: devuele la cantidad de paradas de una determinada linea de colectivos de todos los barrios
+	 * post: devuelve la cantidad de paradas de una determinada linea de colectivos de todos los barrios
 	 * de la ciudad
 	 */
 
-	int cantidadParadasPorLinea(int lineaColectivo,Lista<Barrio>* barrios){
-		Lista<Parada>* paradasPorLinea = obtenerParadasPorLinea(lineaColectivo,barrios);
-		return paradasPorLinea->obtenerCantidad();
-	}
+	int cantidadParadasPorLinea(Lista<Barrio<Parada> >* barrios, int colectivo) {
 
-    Parada paradaMasCercana(Lista<Barrio<Parada> > barrios, double lat, double lon) {
+		Lista<Parada>* paradasPorLinea = obtenerParadasPorColectivo(barrios, colectivo);
 
-        double distancia = numeric_limits<double>::max();
+		return paradasPorLinea->getTamanio();
+	
+    }
+
+    Parada paradaMasCercana(Lista<Barrio<Parada> >* barrios, double lat, double lon) {
+
+        double distancia = std::numeric_limits<double>::max();
         double nueva_distancia;
         Parada parada = Parada("placeholder", 1, 0, 0);
         Parada nueva_parada = Parada("placeholder", 1, 0, 0);
+        Barrio<Parada> barrio = barrios->obtener(1);
 
-        for (int i = 1; i < barrios.ObtenerLargo() + 1; i++) {
+        for (int i = 1; i < barrios->getTamanio() + 1; i++) {
 
-            for (int j = 1; j < barrios.MostrarDatoNodo(i).contarParadas() + 1; j++) {
+            barrio = barrios->obtener(i);
 
-                nueva_parada = barrios.MostrarDatoNodo(i).obtenerParada(j);
+            for (int j = 1; j < barrios->obtener(i).contarParadas() + 1; j++) {
+
+                nueva_parada = barrio.obtenerParada(j);
                 nueva_distancia = sqrt(pow(parada.obtenerLatitud() - lat, 2) + pow(parada.obtenerLongitud() - lon, 2));
                 
                 if (nueva_distancia < distancia) {
@@ -109,36 +141,36 @@ class Asistente{
             
             if (parada.obtenerColectivo() == colectivo) { 
             
-                listaParadas->AgregarNodo(parada, listaParadas->ObtenerLargo() + 1);
+                listaParadas->agregar(parada, listaParadas->getTamanio() + 1);
             
             }
 
         }
 
-        double* distancias = new double[listaParadas->ObtenerLargo()];
+        double* distancias = new double[listaParadas->getTamanio()];
         
-        for (int i = 1; i < listaParadas->ObtenerLargo() + 1; i++) {
+        for (int i = 1; i < listaParadas->getTamanio() + 1; i++) {
             
-            parada = listaParadas->MostrarDatoNodo(i);
+            parada = listaParadas->obtener(i);
             distancias[i] = sqrt(pow(parada.obtenerLatitud() - lat, 2) + pow(parada.obtenerLongitud() - lon, 2));
 
         }
 
         bool desordenado; 
 
-        for (int i = 0; i < listaParadas->ObtenerLargo() - 1; i++) {
+        for (int i = 0; i < listaParadas->getTamanio() - 1; i++) {
             
             desordenado = false;
 
-            for (int j = 0; j < listaParadas->ObtenerLargo() - i - 1; j++) {
+            for (int j = 0; j < listaParadas->getTamanio() - i - 1; j++) {
                 
                 if (distancias[j] > distancias[j + 1]) {
 
-                    swap(distancias[j], distancias[j + 1]);
+                    std::swap(distancias[j], distancias[j + 1]);
                     
-                    parada = listaParadas->MostrarDatoNodo(j + 1);
-                    listaParadas->ModificarDatoNodo(listaParadas->MostrarDatoNodo(j + 2), j);
-                    listaParadas->ModificarDatoNodo(parada, j + 2);
+                    parada = listaParadas->obtener(j + 1);
+                    listaParadas->cambiar(listaParadas->obtener(j + 2), j);
+                    listaParadas->cambiar(parada, j + 2);
                     
                     desordenado = true;
                 
@@ -154,15 +186,16 @@ class Asistente{
 
         }
 
-        cout << listaParadas->MostrarDatoNodo(1).obtenerDireccion(); 
+        std::cout << listaParadas->obtener(1).obtenerDireccion(); 
 
-        for (int i = 2; i < listaParadas->ObtenerLargo() + 1; i++) {
+        for (int i = 2; i < listaParadas->getTamanio() + 1; i++) {
 
-            cout << "; " << listaParadas->MostrarDatoNodo(i).obtenerDireccion();
+            std::cout << "; " << listaParadas->obtener(i).obtenerDireccion();
 
         }
 
     }
+
 };
 /*******************************************************************************************************************
  * Primera aproximación, falta pruebas
